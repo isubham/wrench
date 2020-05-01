0c9d886624679a727c363fdbe8663693db112de9
Manish Kumar Singh <coolmanishks@gmail.com>
2020-05-01 22:59:56 +0530

    added general user homepage

--- android/app/src/main/java/io/gitub/isubham/astra/generalUser/GeneralUserHomeScreen.java
+++ android/app/src/main/java/io/gitub/isubham/astra/generalUser/GeneralUserHomeScreen.java
@@ -3,14 +3,30 @@ package io.gitub.isubham.astra.generalUser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import io.gitub.isubham.astra.R;
 
 public class GeneralUserHomeScreen extends AppCompatActivity {
 
+    Button existing_user,register_user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_user_home_screen);
+        existing_user = (Button)findViewById(R.id.button_existing_user);
+        register_user = (Button)findViewById(R.id.button_register_user);
     }
+    public void ExistingUser(View view){
+
+    }
+
+    public void RegisterUser(View view){
+
+    }
+
+
+
+
 }
