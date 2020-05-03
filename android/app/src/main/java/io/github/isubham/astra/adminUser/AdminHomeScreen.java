52d85ecb315cefd3661abec2c4c1273becd8dd2f
faiz-pitavya <faizrocks9211@gmail.com>
2020-05-04 00:01:45 +0530

    included Scan Activity For Scanner #30

--- android/app/src/main/java/io/github/isubham/astra/adminUser/AdminHomeScreen.java
+++ android/app/src/main/java/io/github/isubham/astra/adminUser/AdminHomeScreen.java
@@ -1,6 +1,8 @@
 package io.github.isubham.astra.adminUser;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -8,8 +10,12 @@ import android.view.WindowManager;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import io.github.isubham.astra.R;
 import io.github.isubham.astra.databinding.AdminHomeScreenBinding;
@@ -22,17 +28,16 @@ public class AdminHomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.admin_home_screen);
 
-        //General Ids
+        adminHomeScreenBinding = AdminHomeScreenBinding.inflate(getLayoutInflater());
+        View view = adminHomeScreenBinding.getRoot();
+        setContentView(view);
+
         findViewByIds();
-        //ProgressBar
-        //showProgressBar();
-        //get the Data from Previous screen before setting things up .
-        getBundleData();
-        //To setup tool bar
         toolbarSetup();
-
+        //showProgressBar();
+        getBundleData();
+        hideProgressBar();
 
     }
 
@@ -44,9 +49,6 @@ public class AdminHomeScreen extends AppCompatActivity {
         }
     }
 
-    /**
-     * Get BundleData From Previous Screen
-     **/
     private void getBundleData() {
     }
 
@@ -82,6 +84,38 @@ public class AdminHomeScreen extends AppCompatActivity {
     public void hideProgressBar() {
         progressBar.setVisibility(View.INVISIBLE);
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+    }
+
+    public void scanCode(View view) {
+        IntentIntegrator intentIntegrator = new IntentIntegrator(AdminHomeScreen.this);
+        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        intentIntegrator.setCameraId(0);
+        intentIntegrator.setOrientationLocked(false);
+        intentIntegrator.setPrompt("Scanning through ASTRA");
+        intentIntegrator.setBeepEnabled(true);
+        intentIntegrator.setBarcodeImageEnabled(true);
+        intentIntegrator.initiateScan();
+
+    }
+
+    public void searchForUser(View view) {
+        if (!TextUtils.isEmpty(adminHomeScreenBinding.adminHomeInputId.getText())) {
+
+            Toast.makeText(AdminHomeScreen.this, "" + adminHomeScreenBinding.adminHomeInputId.getText(), Toast.LENGTH_SHORT).show();
+            adminHomeScreenBinding.adminHomeInputId.setText(null);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (result != null) {
+            if (result.getContents() != null) {
+                Toast.makeText(this, result.getFormatName() + "\n" + result.getContents(), Toast.LENGTH_SHORT).show();
+                super.onActivityResult(requestCode, resultCode, data);
+            }
+        }
     }
 
 }
