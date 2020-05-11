def6ad0a7be801e2f8870763c8062701eddb96d0
MD FAIZ <faizrocx9211@gmail.com>
2020-05-11 20:05:27 +0530

    Fixed Pathway after create general user #13 (#56)
    * Changed Manifest
    
    * Added Space in Manifest
    
    * mid setup before api integration
    
    * Added Custom Snackbar
    
    * Completed Search User #14
    
    * AdminVerifyUser Stuck at viewpager #45
    
    * Finised GeneralUser View Qr Stuff #13
    
    * Fixed Pathway after create user and added shared pref #11
    
    Co-authored-by: faiz-pitavya <faizrocks9211@gmail.com>

---
+++ android/app/src/main/java/io/github/isubham/astra/tools/ResponseCode.java
@@ -0,0 +1,7 @@
+package io.github.isubham.astra.tools;
+
+public class ResponseCode {
+    public static final String AADHAR_EXISTS = "4";
+    public static final String USER_NAME_EXISTS = "5";
+
+}
