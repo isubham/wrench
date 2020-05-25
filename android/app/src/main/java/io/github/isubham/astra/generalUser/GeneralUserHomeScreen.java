98f2712c89e4e289605e8f96980fcc9535edd146
faiz-pitavya <faizrocks9211@gmail.com>
2020-05-26 01:33:34 +0530

    Fixed Ui screens for General User App

--- android/app/src/main/java/io/github/isubham/astra/generalUser/GeneralUserHomeScreen.java
+++ android/app/src/main/java/io/github/isubham/astra/generalUser/GeneralUserHomeScreen.java
@@ -1,44 +1,39 @@
 package io.github.isubham.astra.generalUser;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import io.github.isubham.astra.R;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import io.github.isubham.astra.adminUser.AdminSignIn;
+import io.github.isubham.astra.R;
 import io.github.isubham.astra.tools.Constants;
 import io.github.isubham.astra.tools.LoginPersistance;
 
 
 public class GeneralUserHomeScreen extends AppCompatActivity {
 
-    Button existing_user,register_user;
     private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.general_user_home_screen);
-        existing_user = (Button)findViewById(R.id.button_existing_user);
-        register_user = (Button)findViewById(R.id.button_register_user);
-
 
         findViewByIds();
         toolbarSetup();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.general_user_home_screen_menu, menu);
-        if(LoginPersistance.GetGeneralUserName(this)==null){
+        if (LoginPersistance.GetGeneralUserName(this) == null) {
             menu.findItem(R.id.removemyqr).setVisible(false);
         }
         return true;
@@ -55,16 +50,15 @@ public class GeneralUserHomeScreen extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.myqr:
-                if(LoginPersistance.GetGeneralUserName(this)!=null) {
+                if (LoginPersistance.GetGeneralUserName(this) != null) {
                     startActivity(new Intent(this, GeneralUserViewQr.class).putExtra(Constants.USER_TYPE, Constants.USER_TYPE_ADMIN));
                     return true;
-                }
-                else{
+                } else {
                     Toast.makeText(this, "Please Save your QR Code by searching through existing user", Toast.LENGTH_LONG).show();
                     return true;
                 }
             case R.id.removemyqr:
-                LoginPersistance.update(null,null,null,null,null,this);
+                LoginPersistance.update(null, null, null, null, null, this);
                 Toast.makeText(this, "MyQR sucessfully removed", Toast.LENGTH_LONG).show();
 
 //                sendStatusForLogout();
@@ -82,22 +76,21 @@ public class GeneralUserHomeScreen extends AppCompatActivity {
     }
 
 
-    public void ExistingUser(View view){
+    public void ExistingUser(View view) {
         // switch to general user search page
-//        showProgressBar();
         Intent i = new Intent(GeneralUserHomeScreen.this, GeneralUserSearchUser.class);
         startActivity(i);
-//        hideProgressBar();
 
     }
 
-    public void RegisterUser(View view){
+    public void RegisterUser(View view) {
 //        showProgressBar();
         Intent i = new Intent(GeneralUserHomeScreen.this, CreateGeneralUser.class);
-        i.putExtra(Constants.USER_TYPE,Constants.USER_TYPE_GENERAL);
+        i.putExtra(Constants.USER_TYPE, Constants.USER_TYPE_GENERAL);
         startActivity(i);
 //        hideProgressBar();
     }
+
     private void toolbarSetup() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -105,6 +98,7 @@ public class GeneralUserHomeScreen extends AppCompatActivity {
             toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimaryDark));
         }
     }
+
     private void findViewByIds() {
         progressBar = findViewById(R.id.progressBar);
     }
@@ -119,7 +113,6 @@ public class GeneralUserHomeScreen extends AppCompatActivity {
         progressBar.setVisibility(View.INVISIBLE);
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
     }
-
 
 
 }
